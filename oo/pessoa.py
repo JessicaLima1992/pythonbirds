272179f32c88be129c45731de35f32fb1562f7d9
Jessica <jessica_is2@hotmail.com>
2020-04-13 19:36:34 -0300

    Implementada Direção

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 
 if __name__ == '__main__':
     jessica = Pessoa(nome='Jessica')
@@ -31,3 +39,5 @@ if __name__ == '__main__':
     print(jardson.olhos)
     print(jessica.olhos)
     print(id(Pessoa.olhos), id(jardson.olhos), id(jessica.olhos))
+    print(Pessoa.metodo_estatico(), jardson.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), jardson.nome_e_atributos_de_classe())
